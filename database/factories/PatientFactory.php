63404a36e6a39faea5dd99a9ea1c3027b5b6a42d
JordiGis <jorgisfer@alu.edu.gva.es>
2025-02-02 22:49:57 +0100

    ADD model patient ADD seeder ADD factory

---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Enums\PrefixPhone;
+use App\Models\{Patient, Zone};
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            /*
+            name": "Joan",
+            "lastName": "Garcia",
+            "birthDate": "1950-06-15",
+            "address": {
+                "street": "Carrer Major",
+                "number": "12",
+                "floor": "3r",
+                "door": "2a",
+                "postalCode": "08001",
+                "city": "Barcelona",
+                "province": "Barcelona",
+                "country": "Espanya"
+            },
+            "dni": "12345678A",
+            "healthCardNumber": "CAT123456789",
+            "phone": {
+                "prefix": "34",
+                "number": "666123456"
+            },
+            "email": "joan.garcia@example.com",
+            "zoneId": 1,
+            "situationPersonalFamily": "Viu sol, rep visites del seu fill cada cap de setmana.",
+            "healthSituation": "Hipertensió, medicació diària, risc de caigudes.",
+            "housingSituation": {
+                "type": "Pis",
+                "status": "Bona conservació",
+                "numberOfRooms": 4,
+                "location": "Centre ciutat"
+            },
+            "personalAutonomy": "Necessita ajuda per activitats domèstiques.",
+            "economicSituation": "Pensió mínima, sense copagament.",
+            "emergencyContacts": [
+                {
+                    "name": "Pere",
+                    "lastName": "Garcia",
+                    "phone": {
+                        "prefix": "34",
+                        "number": "677889900"
+                    },
+                    "relationship": "Fill"
+                }
+            ]
+            */
+
+            'name' => $this->faker->name(),
+            'lastName' => $this->faker->lastName(),
+            'birthDate' => $this->faker->dateTimeBetween('-80 years', '-40 years')->format('Y-m-d'),
+            'addressStreet' => $this->faker->streetName(),
+            'addressNumber' => $this->faker->buildingNumber(),
+            'addressFloor' => $this->faker->randomElement(['1r', '2n', '3r', '4t', '5è']),
+            'addressDoor' => $this->faker->randomElement(['1a', '2a', '3a', '4a', '5a']),
+            'addressPostalCode' => $this->faker->numerify('#####'),
+            'addressCity' => $this->faker->city(),
+            'addressProvince' => $this->faker->state(),
+            'addressCountry' => $this->faker->country(),
+            'dni' => $this->faker->unique()->numerify('#########A'),
+            'healthCardNumber' => $this->faker->unique()->bothify('???#########'),
+            'prefix' => $this->faker->randomElement(PrefixPhone::getValues())->value,
+            'phone' => $this->faker->numerify('#########'),
+            'email' => $this->faker->unique()->safeEmail(),
+            'zoneId' => Zone::all()->random()->id,
+            'situationPersonalFamily' => $this->faker->sentence(),
+            'healthSituation' => $this->faker->sentence(),
+            'housingSituationType' => $this->faker->randomElement(['Pis', 'Casa', 'Xalet', 'Apartament', 'Estudi']),
+            'housingSituationStatus' => $this->faker->randomElement(['Bona conservació', 'Molt bona conservació', 'Necessita reformes', 'Molt bona ubicació', 'Molt bona orientació']),
+            'housingSituationNumberOfRooms' => $this->faker->numberBetween(1, 10),
+            'housingSituationLocation' => $this->faker->randomElement(['Centre ciutat', 'Perifèria', 'Muntanya', 'Mar', 'Camp']),
+            'personalAutonomy' => $this->faker->sentence(),
+            'economicSituation' => $this->faker->sentence(),
+        ];
+    }
+}
