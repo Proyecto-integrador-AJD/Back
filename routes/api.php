455b5299c4f43838554ab9d8a665733a74f35d2c
JordiGis <jorgisfer@alu.edu.gva.es>
2025-02-07 00:06:06 +0100

    ADD api alerts

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api\{AuthController, PatientController, ZoneController};
+use App\Http\Controllers\Api\{AuthController, PatientController, ZoneController, AlertController};
 
 
 Route::get('/user', function (Request $request) {
@@ -21,6 +21,7 @@ Route::post('register', [AuthController::class, 'register'])->middleware('api');
 Route::middleware(['auth:sanctum','api'])->group( function () {
     Route::apiResource('patients',  PatientController::class);
     Route::apiResource('zones',  ZoneController::class);
+    Route::apiResource('alerts',  AlertController::class);
 
     Route::post('logout', [AuthController::class, 'logout']);
 });
