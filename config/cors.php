7b34699098c005bf59eea40c57e559b87718c648
Damián Tortosa <gcasual160@gmail.com>
2025-02-20 19:29:00 +0100

    ADD cors fix

--- config/cors.php
+++ config/cors.php
@@ -6,8 +6,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], // O puedes poner tu dominio: ['http://localhost:5173']
-
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174'], 
+    
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
